e2780fea76b3d7d6384d35e439d12aff29e38a43
hungdev <23130126@st.hcmuaf.edu.vn>
2025-12-02 22:26:51 +0700

    làm lại urls

--- ROP/app/urls.py
+++ ROP/app/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('', views.index),
+    path('admin_filter', views.filter),
+    path('admin_post_detail', views.post_detail),
+    path('login', views.login),
+    path('home', views.homeUser),
+    path('ChangePassword', views.ChangePassword),
+    path('user_detailPost', views.detailPost),
+    path('user_personalprofile', views.personalprofile),
 ]
