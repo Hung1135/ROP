7022ea44d1bc362ca4e75c7899ce996bd264c3a7
hungdev <23130126@st.hcmuaf.edu.vn>
2025-12-02 22:26:51 +0700

    làm lại urls

--- ROP/app/views.py
+++ ROP/app/views.py
@@ -2,5 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return render(request, 'admin/home.html')
+#admin
+def filter(request):
+    return render(request, 'admin/ad_filter.html')
+
+def post_detail(request):
+    return render(request, 'admin/post_detail.html')
+
+#login
+def login(request):
+    return render(request, 'login/login.html')
+
+# user
+def homeUser(request):
+    return render(request, 'user/home.html')
+
+def ChangePassword(request):
+    return render(request, 'user/ChangePassword.html')
+
+def detailPost(request):
+    return render(request, 'user/detailPost.html')
+
+def personalprofile(request):
+    return render(request, 'user/personalprofile.html')
