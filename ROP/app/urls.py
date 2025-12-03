b71fea1d7ad812c05a6a8058ee9800c8e6a91ffe
hungdev <23130126@st.hcmuaf.edu.vn>
2025-12-03 14:18:57 +0700

    note

--- ROP/app/urls.py
+++ ROP/app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('admin_filter', views.filter),
     path('admin_post_detail', views.post_detail),
     path('login', views.login),
-    path('home', views.homeUser),
+    path('home_user', views.homeUser),
     path('ChangePassword', views.ChangePassword),
     path('user_detailPost', views.detailPost),
     path('user_personalprofile', views.personalprofile),
