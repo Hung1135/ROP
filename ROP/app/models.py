7c61c60c112671a2e64762f1f5dbdeecd7654d61
2130064-TruongCaoDoan <23130064@st.hcmuaf.edu.vn>
2025-12-10 18:46:31 +0700

    fix

--- ROP/app/models.py
+++ ROP/app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class users (models.Model):
+    id=models.AutoField(primary_key=True)
+    fullname=models.CharField(max_length=100)
+    email=models.CharField(max_length=100)
+    phone=models.CharField(max_length=100)
+    password_hash=models.CharField(max_length=100)
+    role=models.BooleanField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.fullname
+    class Meta:
+        db_table = 'users'
+        managed = False
+
