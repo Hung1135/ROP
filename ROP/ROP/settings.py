6affabf4364e46c616bbbe4a8ea6e284161ec0bd
2130064-TruongCaoDoan <23130064@st.hcmuaf.edu.vn>
2025-12-10 18:46:31 +0700

    fix

--- ROP/ROP/settings.py
+++ ROP/ROP/settings.py
@@ -75,9 +75,16 @@ WSGI_APPLICATION = 'ROP.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'python',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        }
     }
 }
 
