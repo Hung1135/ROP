8684c1fc9fafef4e9646c979033575c26ed33317
TranThiThuyKieu <23130165@st.hcmuaf.edu.vn>
2025-12-13 15:34:22 +0700

    login

--- ROP/app/views.py
+++ ROP/app/views.py
@@ -32,13 +32,85 @@ def manaPostCV(request):
             return redirect('login')
     return render(request, 'admin/managePostCV.html')
 
-
+#logout
+def logout_user(request):
+    if 'user_id' in request.session:
+        del request.session['user_id']
+    if 'user_full_name' in request.session:
+        del request.session['user_full_name']
+    if 'user_role' in request.session:
+        del request.session['user_role']
+    return redirect('login')
 # login
 def login(request):
+    if 'user_id' in request.session:
+        user_role = request.session.get('user_role') 
+        
+        if user_role:
+            return redirect('ListJob') 
+        else:
+            return redirect('home')
+    if request.method == 'POST' and 'full_name' in request.POST:
+        
+        full_name = request.POST.get('full_name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password') 
+        selected_role = request.POST.get('role', 'candidate')
+        if selected_role == 'employer':
+            is_admin = True
+        else:
+            is_admin = False
+        
+        if password != confirm_password:
+            messages.error(request, "Mật khẩu và xác nhận mật khẩu không khớp!")
+            return render(request, 'login/login.html') 
+
+        hashed_password = make_password(password)
+
+        try:
+            users.objects.create( 
+                fullname=full_name, 
+                email=email,
+                phone=phone,
+                password_hash=hashed_password,
+                role=is_admin 
+            )
+            messages.success(request, "Đăng ký thành công! Vui lòng Đăng nhập.")
+            return redirect('login') 
+        except IntegrityError:
+            messages.error(request, "Email hoặc Số điện thoại đã tồn tại. Vui lòng thử lại.")
+            return render(request, 'login/login.html')
+        except Exception as e:
+            messages.error(request, f"Đã có lỗi xảy ra trong quá trình đăng ký: {e}")
+            return render(request, 'login/login.html')
+
+    elif request.method == 'POST' and 'email_login' in request.POST:
+        email = request.POST.get('email_login') 
+        password = request.POST.get('password_login') 
+
+        try:
+            user = users.objects.get(email=email) 
+        except users.DoesNotExist:
+            messages.error(request, "Email hoặc mật khẩu không đúng.")
+            return render(request, 'login/login.html')
+
+        if check_password(password, user.password_hash):
+        
+            request.session['user_id'] = user.id
+            request.session['user_full_name'] = user.fullname 
+            request.session['user_role'] = user.role 
+            
+            if user.role: 
+                return redirect('ListJob') 
+            else: 
+                return redirect('home')
+        else:
+            messages.error(request, "Email hoặc mật khẩu không đúng.")
+            return render(request, 'login/login.html')
 
     return render(request, 'login/login.html')
-
-
 # user
 def homeUser(request):
     if request.method == 'GET':
