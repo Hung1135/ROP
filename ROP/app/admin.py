f4eb2cd4b129e1f6ffa5c2b7a61322717cfdbc7d
2130064-TruongCaoDoan <23130064@st.hcmuaf.edu.vn>
2026-01-17 14:32:38 +0700

    kkk

--- ROP/app/admin.py
+++ ROP/app/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import users, Cvs, Job, Applications
+
+@admin.register(users)
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ('id', 'fullname', 'email', 'phone', 'role', 'created_at')
+    search_fields = ('fullname', 'email', 'phone')
+    list_filter = ('role',)
+
+@admin.register(Cvs)
+class CvsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'full_name', 'email', 'phone', 'uploaded_at')
+    search_fields = ('full_name', 'email')
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'company', 'location', 'category', 'end_date')
+    search_fields = ('title', 'company', 'skills')
+    list_filter = ('category',)
+
+@admin.register(Applications)
+class ApplicationsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'job', 'status', 'applied_at', 'is_sent')
+    list_filter = ('status', 'is_sent')
